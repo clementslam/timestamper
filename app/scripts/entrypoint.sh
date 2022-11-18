f1e556aaeb0455c117d194ae440cdc5ae42f2f11
clementslam <118547001+clementslam@users.noreply.github.com>
2022-11-18 16:27:46 +0100

    Update entrypoint.sh

--- app/scripts/entrypoint.sh
+++ app/scripts/entrypoint.sh
@@ -1,6 +1,4 @@
 #! /bin/sh
-dos2unix 
-if [ "${LOCALDOMAIN}" != ""  ]; then 
-  echo "search ${LOCALDOMAIN}" >> /etc/resolv.conf; 
- fi
+
+if [ "${LOCALDOMAIN}" != ""  ]; then echo "search ${LOCALDOMAIN}" >> /etc/resolv.conf; fi
 exec "$@" 
